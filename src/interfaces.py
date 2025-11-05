96f5c0d3caf67ee260887184b2e5b9ea99e24989
U-1055 <filatov_truba@mail.ru>
2025-11-05 22:16:10 +0700

    Определён интерфейс для DataModel

--- src/interfaces.py
+++ src/interfaces.py
@@ -38,29 +38,123 @@ class IView(ABC):
 
 
 class IModel(ABC):
+    def __init__(self, notes: Path, notes_data: Path, common_data_path: Path, resources: Path,
+                 data_struct: DataStructConst):
+        pass
+
 
     @abstractmethod
-    def get_note_data(self, note: str):
+    @property
+    def notes(self) -> Path:
+        """Возвращает путь к папке с заметками."""
         pass
 
     @abstractmethod
-    def get_note_tags(self, note: str) -> str:
+    @property
+    def notes_data(self) -> Path:
+        """Возвращает путь к базе данных."""
+        pass
+
+    @abstractmethod
+    @property
+    def resources(self) -> Path:
+        """Возвращает путь к файлу ресурсов."""
+        pass
+
+    @abstractmethod
+    @property
+    def data_struct(self) -> DataStructConst:
+        """Возвращает экземпляр класса набора констант DataStructConst."""
+        pass
+
+    @abstractmethod
+    def validate_files(self) -> tuple[str, ...]:
+        """Проверяет файлы на соответствие структуре (notes и DBB)."""
+        pass
+
+    @abstractmethod
+    def reclaim_common_data(self, common_data: dict) -> dict:
+        """Восстанавливает common_data при наличии повреждений."""
+        pass
+
+    @abstractmethod
+    def reclaim_note(self, note: str):
+        pass
+
+    @abstractmethod
+    def get_style(self, style: str) -> str:
+        """
+        Возвращает стиль QSS.
+        :param style: путь к стилю
+        """
+        pass
+
+    @abstractmethod
+    def get_last_style(self) -> str:
+        """Возвращает установленный пользователем стиль (из config.json)"""
+        pass
+
+    @abstractmethod
+    def get_note_tags(self, note: str) -> tuple[str, ...]:
         pass
 
     @abstractmethod
     def get_tags(self) -> tuple[str, ...]:
+        """Возвращает все теги."""
         pass
 
     @abstractmethod
-    def get_note_content(self, note: str):
+    def get_notes_tags(self) -> tuple[str, ...]:
+        """Возвращает теги, к которым прикреплены заметки."""
         pass
 
     @abstractmethod
-    def get_note_date_changing(self, note: str):
+    def get_note_content(self, note: str) -> str:
         pass
 
+    @abstractmethod
+    def get_note_date_changing(self, note: str) -> str:
+        pass
+    @abstractmethod
     def get_notes(self) -> tuple:
         """Возвращает список со всеми заметками"""
+        pass
+
+    @abstractmethod
+    def set_note_date_changing(self, note: str, date_changing: str):
+        pass
+
+    @abstractmethod
+    def set_note_tags(self, note: str, tags: list[str] | tuple[str, ...]):
+        pass
+
+    @abstractmethod
+    def set_note_content(self, note: str, content: str):
+        pass
+
+    @abstractmethod
+    def delete_note(self, note: str):
+        pass
+
+    @abstractmethod
+    def change_note_name(self, note: str, name: str):
+        """
+        Изменяет название заметки.
+        :param note: текущее название заметки.
+        :param name: новое название.
+        """
+        pass
+
+    @abstractmethod
+    def add_tag(self, tag: str):
+        pass
+
+    @abstractmethod
+    def delete_tag(self, tag: str):
+        pass
+
+    @abstractmethod
+    def add_note(self, name: str, tags: list[str] | tuple[str, ...]):
         pass
 
 
